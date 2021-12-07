aef9a631f6dcfa2f6185ca2e3ace5247a1ac2bdc
jessicawaz <jesswaz05@gmail.com>
2021-12-07 10:54:12 -0500

    contact form stylin

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,57 @@
 import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="contact-container">
+      <h1>Contact Me.</h1>
+      <form>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label for="name">Your Name</label>
+              <input
+                type="text"
+                class="form-control"
+                id="name"
+                placeholder="Name..."
+              />
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="form-group">
+              <label for="email">Your Email</label>
+              <input
+                type="email"
+                class="form-control"
+                id="email"
+                placeholder="Email..."
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-6 mx-auto">
+            <div class="form-group">
+              <label for="message">Your Message</label>
+              <textarea
+                class="form-control"
+                id="message"
+                rows="3"
+                placeholder="Message..."
+              ></textarea>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <button type="submit" className="btn btn-primary mx-auto">
+            Send Message
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
