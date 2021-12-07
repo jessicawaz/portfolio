4e475cd56d9fdead0ca17a915edd71a68227b5f6
jessicawaz <jesswaz05@gmail.com>
2021-12-07 09:40:52 -0500

    project styling

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,27 +5,29 @@ const Projects = () => {
   return (
     <div className="projects">
       <h1>Projects.</h1>
-      <div className="project-cards">
-        <div className="card-item">
-          <h4>
-            <b>Inventory Tracker</b>
-          </h4>
-          <p>Created using the MERN stack.</p>
-          <a href="#" target="_blank">
-            Source Code
-          </a>
+      <div className="projects-row">
+        <div className="project-column">
+          <div className="card-item">
+            <h4>
+              <b>Inventory Tracker</b>
+            </h4>
+            <p>Created using the MERN stack.</p>
+            <a href="#" target="_blank">
+              Source Code
+            </a>
+          </div>
         </div>
-      </div>
 
-      <div className="project-cards">
-        <div className="card-item">
-          <h4>
-            <b></b>
-          </h4>
-          <p>Created using the MERN stack.</p>
-          <a href="#" target="_blank">
-            Source Code
-          </a>
+        <div className="project-column">
+          <div className="card-item">
+            <h4>
+              <b>Inventory Tracker</b>
+            </h4>
+            <p>Created using the MERN stack.</p>
+            <a href="#" target="_blank">
+              Source Code
+            </a>
+          </div>
         </div>
       </div>
     </div>
