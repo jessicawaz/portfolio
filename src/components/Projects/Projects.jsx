4fd63292eb34270192e20f77d2f76d8e3d963d3b
jessicawaz <jesswaz05@gmail.com>
2021-12-12 14:36:23 -0500

    projects styling

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,28 +5,42 @@ const Projects = () => {
   return (
     <div className="projects">
       <h1>Projects.</h1>
-      <div className="projects-row">
-        <div className="project-column">
-          <div className="card-item">
-            <h4>
-              <b>Inventory Tracker</b>
-            </h4>
-            <p>Created using the MERN stack.</p>
-            <a href="#" target="_blank">
-              Source Code
-            </a>
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Inventory Tracker</h5>
+              <p class="card-text">Inventory app built with the MERN stack.</p>
+              <a href="#" class="btn btn-primary">
+                Source Code
+              </a>
+            </div>
           </div>
         </div>
 
-        <div className="project-column">
-          <div className="card-item">
-            <h4>
-              <b>Inventory Tracker</b>
-            </h4>
-            <p>Created using the MERN stack.</p>
-            <a href="#" target="_blank">
-              Source Code
-            </a>
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">This Portfolio</h5>
+              <p class="card-text">Portfolio built with React.</p>
+              <a href="#" class="btn btn-primary">
+                Source Code
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Requirements Engineering</h5>
+              <p class="card-text">Created an SRS with Jira/Confluence.</p>
+              <a href="#" class="btn btn-primary">
+                View Documents
+              </a>
+            </div>
           </div>
         </div>
       </div>
