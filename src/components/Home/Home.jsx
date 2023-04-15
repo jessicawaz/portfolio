f294489f1170d96e64cae1555451ae7169ada36e
jesswazbinski <jesswaz05@gmail.com>
2023-04-15 15:10:15 -0400

    WIP

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,42 +6,48 @@ const Home = () => {
   return (
     <div className="home">
       <div>
-        <h1 className="home-name">Jessica Wazbinski.</h1>
+        <h1 className="home-name">Jessica Wazbinski</h1>
         <h2 className="home-desc">Software Developer</h2>
 
         <div className="container">
           <div className="row">
             <div className="col">
-              <a
-                href="https://www.linkedin.com/in/jessica-wazbinski/"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <i class="fab fa-linkedin icons"></i>
-              </a>
-              <p>LinkedIn</p>
+              <div className="icon">
+                <a
+                  href="https://www.linkedin.com/in/jessica-wazbinski/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <i class="fab fa-linkedin icons"></i>
+                </a>
+                <p>LinkedIn</p>
+              </div>
             </div>
 
             <div className="col">
-              <a
-                href="https://github.com/jessicawaz"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <i class="fab fa-github-square icons"></i>
-              </a>
-              <p>GitHub</p>
+              <div className="icon">
+                <a
+                  href="https://github.com/jessicawaz"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <i class="fab fa-github-square icons"></i>
+                </a>
+                <p>GitHub</p>
+              </div>
             </div>
 
             <div className="col">
-              <a
-                href="https://drive.google.com/file/d/1eRjiQjvQq8oCV97Otwpiwpg5_G7Zx2WC/view?usp=sharing"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <i class="fas fa-file icons"></i>
-              </a>
-              <p>Resume</p>
+              <div className="icon">
+                <a
+                  href="https://drive.google.com/file/d/1eRjiQjvQq8oCV97Otwpiwpg5_G7Zx2WC/view?usp=sharing"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <i class="fas fa-file icons"></i>
+                </a>
+                <p>Resume</p>
+              </div>
             </div>
           </div>
         </div>
